a1975bc645276a0c3db736f7e49dc68261535642
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2024-10-10 20:16:56 -0300

    feat: Firebase Crashlytics configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -8,10 +10,19 @@ import 'package:keener_notes/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterError.onError = (flutterErrorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(flutterErrorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack);
+    return true;
+  };
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
+
   runApp(
     ModularApp(
       module: AppModule(),
