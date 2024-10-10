a95476c36a74b5cde58d3ebd14981d529e63bd79
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2024-10-10 19:36:03 -0300

    feat(update_note_module): Update note repository created

--- lib/app/modules/update_note/update_note_module.dart
+++ lib/app/modules/update_note/update_note_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_notes/app/modules/update_note/repository/update_note_repository.dart';
 import 'package:keener_notes/app/modules/update_note/service/update_note_service.dart';
 import 'package:keener_notes/app/modules/update_note/update_note_page.dart';
 import 'package:keener_notes/app/modules/update_note/update_note_store.dart';
@@ -7,7 +8,9 @@ class UpdateNoteModule extends Module {
   @override
   void binds(i) {
     i.addSingleton<UpdateNoteStore>(UpdateNoteStore.new);
+
     i.addSingleton<UpdateNoteService>(UpdateNoteService.new);
+    i.addSingleton<UpdateNoteRepository>(UpdateNoteRepositoryImpl.new);
   }
 
   @override
--- lib/app/modules/update_note/update_note_page.dart
+++ lib/app/modules/update_note/update_note_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_notes/app/modules/update_note/service/update_note_service.dart';
 import 'package:keener_notes/app/shared/mdoels/note_model.dart';
+import 'package:keener_notes/app/shared/widgets/custom_button_widget.dart';
 
 class UpdateNotePage extends StatefulWidget {
   final NoteModel note;
@@ -13,6 +16,8 @@ class UpdateNotePage extends StatefulWidget {
 }
 
 class _UpdateNotePageState extends State<UpdateNotePage> {
+  final UpdateNoteService _noteService = Modular.get<UpdateNoteService>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +26,19 @@ class _UpdateNotePageState extends State<UpdateNotePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(widget.note.title),
+            Text(widget.note.body),
+            Text(widget.note.id!),
+            CustomButtonWidget(
+              text: 'update',
+              onPressed: () async {
+                await _noteService.updateNote(
+                  noteId: widget.note.id!,
+                  title: 'Atualizado',
+                  body: 'Muito atualizado',
+                );
+                Modular.to.navigate('/noteslist/');
+              },
+            ),
           ],
         ),
       ),
