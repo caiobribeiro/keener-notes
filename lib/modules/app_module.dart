843213351591919c86261f7a736e5bfb11ae838e
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2024-10-10 08:30:41 -0300

    feat: SigninModule created

--- lib/modules/app_module.dart
+++ lib/modules/app_module.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_notes/modules/signin/signin_module.dart';
+import 'package:keener_notes/modules/signup/signup_module.dart';
 
 class AppModule extends Module {
   @override
   void binds(i) {}
 
   @override
-  void routes(r) {}
+  void routes(r) {
+    r.module('/signup', module: SignupModule());
+    r.module('/signin', module: SigninModule());
+  }
 }
