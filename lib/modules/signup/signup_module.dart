84f1cbbf93bd281e7056bd9fc3882b1a74a58909
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2024-10-10 08:29:16 -0300

    feat: SignupModule created

---
+++ lib/modules/signup/signup_module.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_notes/modules/signup/signup_page.dart';
+import 'package:keener_notes/modules/signup/signup_store.dart';
+
+class SignupModule extends Module {
+  @override
+  void binds(i) {
+    i.addSingleton<SignupStore>(SignupStore.new);
+  }
+
+  @override
+  void routes(r) {
+    r.child(
+      '/',
+      child: (context) => const SignupPage(),
+    );
+  }
+}
